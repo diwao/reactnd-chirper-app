02a73c4c4c09cb944f099c9cc5bee77ec5729491
Daisuke Iwao <nakapom@gmail.com>
2021-01-30 22:54:58 +0900

    fix: Fix bug

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -7,12 +7,10 @@ const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData() {
   return (dispatch) => {
-    return () => {
-      return getInitialData().then(({ users, tweets }) => {
-        dispatch(receiveUsers(users));
-        dispatch(receiveTweets(tweets));
-        dispatch(setAuthedUser(AUTHED_ID));
-      });
-    };
+    return getInitialData().then(({ users, tweets }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
   };
 }
