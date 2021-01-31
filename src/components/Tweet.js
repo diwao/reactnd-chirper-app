b97e4e4c1048d033f392cc08d324dfb45ec3f1ab
Daisuke Iwao <nakapom@gmail.com>
2021-01-31 18:03:20 +0900

    feat: Add parent tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,17 +4,25 @@ import { formatTweet } from '../utils/helpers';
 
 class Tweet extends Component {
   render() {
-    console.log(this.props);
+    const { tweet } = this.props;
+
+    if (tweet == null) {
+      return <p>This tweet dosen't exists</p>;
+    }
+
     return <div className="tweet"></div>;
   }
 }
 
 function mapStateToProps({ authedUser, users, tweets }, { id }) {
   const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser),
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null,
   };
 }
 
