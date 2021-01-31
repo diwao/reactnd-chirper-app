d81d17c5d3afd4574db2efdf4830475e7e094754
Daisuke Iwao <nakapom@gmail.com>
2021-01-31 22:20:18 +0900

    feat: Implement like tweet feature

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -6,12 +6,16 @@ import {
   TiHeartOutline,
   TiHeartFullOutline,
 } from 'react-icons/ti';
+import { handleToggleTweet } from '../actions/tweets';
 
 class Tweet extends Component {
   handleLike = (e) => {
     e.preventDefault();
 
-    // todo: handle like tweet
+    const { dispatch, tweet, authedUser } = this.props;
+    dispatch(
+      handleToggleTweet({ id: tweet.id, authedUser, hasLiked: tweet.hasLiked })
+    );
   };
 
   toParent = (e, id) => {
